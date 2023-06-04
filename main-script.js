9c76067caa9d168fd6ba99562726c923bbcb4c6f
AlphaBMJy <79057563+AlphaBMJy@users.noreply.github.com>
2023-06-04 13:09:32 +0200

    first upload

---
+++ main-script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var userLang = navigator.language || navigator.userLanguage;    if (userLang === 'de' || userLang === 'de-DE') {
+        var popupContainer = document.getElementById('popup-container');
+        popupContainer.style.display = 'block';
+    }
+});
+
+function closePopup() {
+    var popupContainer = document.getElementById('popup-container');
+    popupContainer.style.display = 'none';
+}
+
+
+function notGerman() {
+    closePopup();
+    var notification = document.getElementById("notification");
+    notification.style.display = "block";
+  
+    setTimeout(function() {
+     notification.style.display = "none";
+      }, 3000);
+  }
+
+
